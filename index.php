90c54f36b4c281fa8d465b7f0c91b7dfe33b2217
fullstack82 <fullstack8210@gmail.com>
2021-01-28 10:08:49 +0100

    constantes predifinidas

--- index.php
+++ index.php
@@ -17,5 +17,15 @@ echo '<h1>'.web.'</h1>'; // Una constante concatenada;
 
 // Constantes predefinidas
 
+// echo PHP_OS;     // Nos mostraria el sistema operativo que estamos usando;
 
+// echo PHP_VERSION;    // Nos mostrara el numero de la version de PHP;
+
+// echo PHP_EXTENSION_DIR; // Nos muestra donde estan las extensiones aqui instaladas;
+
+// echo __LINE__ ;  // Nos muestra el numero de linea donde estamos imprimiendo este valor;
+
+// echo __FILE__ ;    // Nos muestra la ruta directa de este archivo ;
+
+// echo __FUNCTION__ ;    // Nos muestra el nombre de la función que estamos usando;
 
