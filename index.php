100f251b85c6bb44b9dfadf78ecaaf63cfa729ef
fullstack82 <fullstack8210@gmail.com>
2021-01-28 09:55:31 +0100

    constantes

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
 
 
-//Constantes
+// Constantes
+// Es un contenedor de información que no puede variar 
+
+
+// Como se define una constante;
+
+define('nombre', 'María');
+define('web', 'master');
+
+
+echo nombre; // Una constante se muestra sin el simbolo $, solo el nombre de la constante;
+echo '<h1>'.web.'</h1>'; // Una constante concatenada;
+
+
+// Constantes predefinidas
+
+
+
